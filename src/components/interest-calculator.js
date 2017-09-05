096ef5fab7c74e83fa919df6f3c27019f8d6cef9
jeffreytoconnell <jeffreytoconnell@gmail.com>
2017-09-05 15:56:30 -0700

    added import

--- src/components/interest-calculator.js
+++ src/components/interest-calculator.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import {connect} from 'react-redux';
 
+import {setPrincipal, setInterest, setYears} from '../actions';
+
 export function InterestCalculator(props) {
     return (
         <form className="interest-calculator"
